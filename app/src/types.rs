5824931ef076111658d215d50a761d477196fa7a
Guillaume Depardon <guillaume.depardon@gmail.com>
2018-03-11 08:36:17 +0100

    Added json types

--- app/src/types.rs
+++ app/src/types.rs
@@ -22,3 +22,26 @@ pub fn player_channel() -> (PlayerSender, PlayerReceiver) {
 pub fn gui_channel() -> (GuiSender, GuiReceiver) {
     mpsc::channel()
 }
+
+#[derive(Deserialize)]
+pub struct Resolution {
+    pub height: u32,
+    pub width: u32,
+}
+
+#[derive(Deserialize)]
+pub struct PlaylistInfo {
+    pub url: String,
+    pub name: String,
+    pub bandwidth: u64,
+    pub resolution: Resolution,
+}
+
+#[derive(Deserialize)]
+pub struct PlaylistsInfo {
+    pub channel: String,
+    pub playlists: Vec<PlaylistInfo>,
+}
+
+pub type SegmentUrl = String;
+pub type Playlist = Vec<SegmentUrl>;
