3526b45583b185454a592936500630fbec83d988
Guillaume Depardon <guillaume.depardon@gmail.com>
2018-03-11 19:34:38 +0100

    Centralized command logic in a module

---
+++ app/src/process.rs
@@ -0,0 +1,36 @@
+use std::process::{Command, Child, Stdio};
+use std::io::Result;
+
+const VLC_PATH: &'static str = r"C:\Program Files (x86)\VideoLAN\VLC\vlc.exe";
+const CHROME_PATH: &'static str = r"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe";
+
+pub fn vlc_title(channel: &str) -> String {
+    format!("Twitch-Player_{}", channel)
+}
+
+pub fn run_vlc(channel: &str) -> Result<Child> {
+    let args = [
+        "-",
+        "--qt-minimal-view", "--no-qt-video-autoresize",
+        "--key-faster=ctrl+f", "--key-rate-normal=ctrl+n",
+        "--file-caching=3000", "--live-caching=3000",
+        "--disc-caching=3000", "--network-caching=3000",
+        &format!("--meta-title={}", vlc_title(channel))
+    ];
+
+    Command::new(VLC_PATH)
+        .args(&args)
+        .stdin(Stdio::piped())
+        .spawn()
+}
+
+pub fn run_chat(channel: &str) -> Result<Child> {
+    let url = format!("https://www.twitch.tv/popout/{}/chat?popout=", channel);
+    let args = [
+        &format!("--app={}", url)
+    ];
+
+    Command::new(CHROME_PATH)
+        .args(&args)
+        .spawn()
+}
