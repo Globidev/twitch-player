1fb0c5cae79bcae348ed53626d0037f29dfc1702
Guillaume Depardon <guillaume.depardon@gmail.com>
2018-03-14 00:48:59 +0100

    refactored the win32 specific calls

--- app/src/windows.rs
+++ app/src/windows.rs
@@ -1,12 +1,51 @@
 extern crate winapi;
 
 use self::winapi::shared::windef::HWND;
-use self::winapi::shared::minwindef::{BOOL, FALSE, LPARAM, TRUE};
-use self::winapi::um::winuser::{EnumWindows, GetWindowTextW};
+use self::winapi::shared::minwindef::{BOOL, LPARAM, DWORD, LPDWORD, FALSE, TRUE};
+use self::winapi::um::winuser::{EnumWindows, GetWindowTextW, GetWindowThreadProcessId};
 use self::winapi::ctypes::c_int;
 
-use std::ptr;
 use std::slice::from_raw_parts;
+
+struct FindWindowContext<F: Fn(HWND) -> bool> {
+    handle: Option<HWND>,
+    predicate: F
+}
+
+extern "system" fn enum_window_callback<F>(handle: HWND, lparam: LPARAM) -> BOOL 
+where 
+    F: Fn(HWND) -> bool
+{
+    let context_ptr = lparam as *mut FindWindowContext<F>;
+
+    if let Some(ref mut context) = unsafe { context_ptr.as_mut() } {
+        if (context.predicate)(handle) {
+            context.handle = Some(handle);
+            return FALSE;
+        }
+    }
+
+    TRUE
+}
+
+pub fn find_window<F>(predicate: F) -> Option<HWND>
+where
+    F: Fn(HWND) -> bool,
+{
+    let mut context = FindWindowContext {
+        handle: None,
+        predicate: predicate
+    };
+
+    unsafe {
+        EnumWindows(
+            Some(enum_window_callback::<F>), 
+            &mut context as *mut FindWindowContext<F> as LPARAM
+        )
+    };
+
+    context.handle
+}
 
 fn get_window_text(handle: HWND) -> Option<String> {
     let mut text_buffer = [0u16; 256];
@@ -28,47 +67,38 @@ fn get_window_text(handle: HWND) -> Option<String> {
     }
 }
 
-struct FindWindowContext<F: FnMut(&str) -> bool> {
-    handle: HWND,
-    predicate: F,
-}
-
-extern "system" fn on_window<F>(handle: HWND, lparam: LPARAM) -> BOOL 
-where 
-    F: FnMut(&str) -> bool
-{
-    let context_ptr = lparam as *mut FindWindowContext<F>;
-
-    if let Some(title) = get_window_text(handle) {
-        if let Some(ref mut context) = unsafe { context_ptr.as_mut() } {
-            if (context.predicate)(&title) {
-                context.handle = handle;
-                return FALSE
-            }
-        }
-    }
-
-    TRUE
-}
-
-pub fn find_window_by_name<F>(pred: F) -> Option<HWND>
+pub fn find_window_by_name<F>(name_predicate: F) -> Option<HWND>
 where
-    F: FnMut(&str) -> bool,
+    F: Fn(String) -> bool,
 {
-    let mut context = FindWindowContext {
-        handle: ptr::null_mut(),
-        predicate: pred
+    let predicate = |handle: HWND| {
+        get_window_text(handle)
+            .map(|name| name_predicate(name))
+            .unwrap_or(false)
     };
 
-    unsafe {
-        EnumWindows(
-            Some(on_window::<F>), 
-            &mut context as *mut FindWindowContext<F> as LPARAM
-        )
-    };
+    find_window(predicate)
+}
 
-    match context.handle.is_null() {
-        true  => None,
-        false => Some(context.handle)
+fn get_window_pid(handle: HWND) -> Option<DWORD> {
+    let mut win_pid: DWORD = 0;
+    unsafe { GetWindowThreadProcessId(handle, &mut win_pid as LPDWORD) };
+
+    match win_pid {
+        0 => None,
+        pid => Some(pid)
     }
+}
+
+pub fn find_window_by_pid<F>(pid_predicate: F) -> Option<HWND> 
+where
+    F: Fn(u32) -> bool,
+{
+    let predicate = |handle: HWND| {
+        get_window_pid(handle)
+            .map(|pid| pid_predicate(pid))
+            .unwrap_or(false)
+    };
+
+    find_window(predicate)
 }
