57d00c532ae3cefb2821e1b84d15d8759e407d48
Guillaume Depardon <guillaume.depardon@gmail.com>
2018-03-18 20:22:00 +0100

    Added an external toml config
    Will be used to customize commands

--- app/src/options.rs
+++ app/src/options.rs
@@ -1,4 +1,13 @@
+extern crate toml;
+
 const PROTOCOL_PREFIX: &'static str = "twitch-player://";
+
+use std::io::{self, Read};
+use std::fs::File;
+use std::fmt;
+use std::error;
+
+use self::toml::from_str as decode;
 
 fn parse_channel(channel: &str) -> String {
     if channel.starts_with(PROTOCOL_PREFIX) {
@@ -10,9 +19,63 @@ fn parse_channel(channel: &str) -> String {
     }
 }
 
+fn parse_config(file_path: &str) -> Result<Config, ConfigError> {
+    let mut raw_config = String::new();
+    let mut file = File::open(file_path)?;
+
+    file.read_to_string(&mut raw_config)?;
+    let config = decode(&raw_config)?;
+
+    Ok(config)
+}
+
 #[derive(StructOpt, Debug, Clone)]
-#[structopt(name = "twitch-player", about = "A native player for Twitch on Windows")]
+#[structopt(name = "twitch-player",
+            about = "A native player for Twitch on Windows")]
 pub struct Options {
-    #[structopt(help = "Channel name or Protocol URI", parse(from_str = "parse_channel"))]
+    #[structopt(help = "Channel name or Protocol URI",
+                parse(from_str = "parse_channel"))]
     pub channel: String,
+    #[structopt(help = "Configuration file",
+                long = "config",
+                default_value="config.toml",
+                parse(try_from_str = "parse_config"))]
+    pub config: Config,
+}
+
+#[derive(Deserialize, Debug, Clone)]
+#[serde(rename_all = "kebab-case")]
+pub struct Config {
+    pub video_player: ProcessConfig,
+    pub chat_renderer: ProcessConfig,
+}
+
+#[derive(Deserialize, Debug, Clone)]
+pub struct ProcessConfig {
+    pub command: String,
+    pub args: Vec<String>,
+}
+
+#[derive(Debug)]
+enum ConfigError {
+    IoError(io::Error),
+    TomlError(toml::de::Error)
+}
+
+impl error::Error for ConfigError {
+    fn description(&self) -> &str { "Configuration Error" }
+}
+
+impl fmt::Display for ConfigError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+impl From<io::Error> for ConfigError {
+    fn from(error: io::Error) -> Self { ConfigError::IoError(error) }
+}
+
+impl From<toml::de::Error> for ConfigError {
+    fn from(error: toml::de::Error) -> Self { ConfigError::TomlError(error) }
 }
