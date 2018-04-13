276033ff4786d3452214694cd500e901311802d5
Guillaume Depardon <guillaume.depardon@gmail.com>
2018-04-13 11:29:13 +0200

    Refactored the m3u8 parsing logic

--- twitchd/src/twitch/m3u8.rs
+++ twitchd/src/twitch/m3u8.rs
@@ -6,21 +6,24 @@ use std::iter::FromIterator;
 use std::collections::HashMap;
 type RawInfoMap = HashMap<String, String>;
 
-use twitch::types::{StreamIndex, PlaylistInfo, TwitchInfo, MediaInfo, MediaType, StreamInfo, Resolution};
+use twitch::types::*;
 
 #[derive(Debug)]
-pub enum ParseError {
-    FormatError(String),
-    ExtractError(ExtractInfoError),
-}
+pub struct ParseError(pub String);
 
 pub fn parse_stream_index(data: &[u8]) -> Result<StreamIndex, ParseError> {
     use nom::IResult::*;
 
     match stream_index_parser(data) {
         Done(_, result) => Ok(result),
-        Error(e) => Err(ParseError::FormatError(e.to_string())),
-        Incomplete(n) => Err(ParseError::FormatError(String::from("incomplete"))),
+        Error(e) => {
+            let detail = format!("Error: {}", e);
+            Err(ParseError(detail))
+        },
+        Incomplete(n) => {
+            let detail = format!("Incomplete: {:?}", n);
+            Err(ParseError(detail))
+        }
     }
 }
 
@@ -28,11 +31,8 @@ fn to_string(data: &[u8]) -> Result<String, Utf8Error> {
     from_utf8(data).map(String::from)
 }
 
-named!(parse_quoted_string, do_parse!(
-    tag!("\"") >>
-    content: take_until!("\"") >>
-    tag!("\"") >>
-    (content)
+named!(parse_quoted_string, delimited!(
+    tag!("\""), take_until!("\""), tag!("\"")
 ));
 
 named!(parse_key_value<(String, String)>, separated_pair!(
@@ -49,22 +49,19 @@ named!(raw_info_parser<RawInfoMap>, map!(
     FromIterator::from_iter
 ));
 
-named!(twitch_info_parser<TwitchInfo>, do_parse!(
-    tag!("#EXT-X-TWITCH-INFO:") >>
-    twitch_info: map_res!(raw_info_parser, extract_twitch_info) >>
-    (twitch_info)
+named!(twitch_info_parser<TwitchInfo>, preceded!(
+    tag!("#EXT-X-TWITCH-INFO:"),
+    map_res!(raw_info_parser, extract_twitch_info)
 ));
 
-named!(media_info_parser<MediaInfo>, do_parse!(
-    tag!("#EXT-X-MEDIA:") >>
-    media_info: map_res!(raw_info_parser, extract_media_info) >>
-    (media_info)
+named!(media_info_parser<MediaInfo>, preceded!(
+    tag!("#EXT-X-MEDIA:"),
+    map_res!(raw_info_parser, extract_media_info)
 ));
 
-named!(stream_info_parser<StreamInfo>, do_parse!(
-    tag!("#EXT-X-STREAM-INF:") >>
-    stream_info: map_res!(raw_info_parser, extract_stream_info) >>
-    (stream_info)
+named!(stream_info_parser<StreamInfo>, preceded!(
+    tag!("#EXT-X-STREAM-INF:"),
+    map_res!(raw_info_parser, extract_stream_info)
 ));
 
 named!(playlist_info_parser<PlaylistInfo>, do_parse!(
@@ -89,9 +86,24 @@ named!(stream_index_parser<StreamIndex>, do_parse!(
 ));
 
 #[derive(Debug)]
-pub enum ExtractInfoError {
+enum ExtractInfoError {
     MissingKey(String),
-    FromError(String)
+    BadValue(BadValueError),
+}
+
+#[derive(Debug)]
+struct BadValueError {
+    raw_value: String,
+    detail: String,
+}
+
+impl BadValueError {
+    fn new(raw_value: &str, detail: &str) -> Self {
+        Self {
+            raw_value: String::from(raw_value),
+            detail: String::from(detail)
+        }
+    }
 }
 
 fn extract_twitch_info(raw_info: RawInfoMap) -> Result<TwitchInfo, ExtractInfoError> {
@@ -119,8 +131,8 @@ fn extract_media_info(raw_info: RawInfoMap) -> Result<MediaInfo, ExtractInfoErro
         type_:      extract_raw(&raw_info, "TYPE")?,
         group_id:   extract_raw(&raw_info, "GROUP-ID")?,
         name:       extract_raw(&raw_info, "NAME")?,
-        autoselect: extract_raw(&raw_info, "AUTOSELECT")?,
-        default:    extract_raw(&raw_info, "DEFAULT")?,
+        autoselect: extract_raw::<BooleanLike>(&raw_info, "AUTOSELECT")?.into(),
+        default:    extract_raw::<BooleanLike>(&raw_info, "DEFAULT")?.into(),
     };
 
     Ok(media_info)
@@ -139,93 +151,79 @@ fn extract_stream_info(raw_info: RawInfoMap) -> Result<StreamInfo, ExtractInfoEr
 }
 
 fn extract_raw<T : Extractable>(raw_info: &RawInfoMap, key: &str) -> Result<T, ExtractInfoError> {
-    if let Some(value) = raw_info.get(key) {
-        Extractable::extract(value)
+    if let Some(raw_value) = raw_info.get(key) {
+        Extractable::extract(raw_value).map_err(ExtractInfoError::BadValue)
     } else {
         Err(ExtractInfoError::MissingKey(key.to_string()))
     }
 }
 
-trait Extractable: Sized {
-    fn extract(string: &String) -> Result<Self, ExtractInfoError>;
-}
+struct BooleanLike(pub bool);
 
-impl Extractable for String {
-    fn extract(string: &String) -> Result<String, ExtractInfoError> {
-        Ok(string.clone())
+impl Into<bool> for BooleanLike {
+    fn into(self) -> bool {
+        let BooleanLike(wrapped) = self;
+        wrapped
     }
 }
 
-impl Extractable for bool {
-    fn extract(string: &String) -> Result<bool, ExtractInfoError> {
-        let lowercased = string.to_lowercase();
-        if lowercased == "true" || lowercased == "yes" {
-            Ok(true)
-        } else if lowercased == "false" || lowercased == "no" {
-            Ok(false)
-        } else {
-            Err(ExtractInfoError::FromError(String::from("Bad boolean value")))
+trait Extractable: Sized {
+    fn extract(s: &String) -> Result<Self, BadValueError>;
+}
+
+use std::str::FromStr;
+use std::fmt::Display;
+
+impl<T> Extractable for T
+where
+    T:                   FromStr,
+    <T as FromStr>::Err: Display
+{
+    fn extract(s: &String) -> Result<T, BadValueError> {
+        s.parse().map_err(|e| {
+            let detail = format!("FromStr error: {}", e);
+            BadValueError::new(s, &detail)
+        })
+    }
+}
+
+impl Extractable for BooleanLike {
+    fn extract(s: &String) -> Result<BooleanLike, BadValueError> {
+        match &s.to_lowercase() as &str {
+            "true"  | "yes" => Ok(BooleanLike(true)),
+            "false" | "no"  => Ok(BooleanLike(false)),
+            _ => Err(BadValueError::new(s, "Invalid boolean-like value"))
         }
     }
 }
 
-use std::net::{IpAddr, AddrParseError};
-
-impl Extractable for IpAddr {
-    fn extract(string: &String) -> Result<IpAddr, ExtractInfoError> {
-        string.parse().map_err(|e: AddrParseError| ExtractInfoError::FromError(e.to_string()))
-    }
-}
-
-impl Extractable for f64 {
-    fn extract(string: &String) -> Result<f64, ExtractInfoError> {
-        use std::num::ParseFloatError;
-
-        string.parse().map_err(|e: ParseFloatError| ExtractInfoError::FromError(e.to_string()))
-    }
-}
-
-impl Extractable for u32 {
-    fn extract(string: &String) -> Result<u32, ExtractInfoError> {
-        use std::num::ParseIntError;
-
-        string.parse().map_err(|e: ParseIntError| ExtractInfoError::FromError(e.to_string()))
-    }
-}
-
-impl Extractable for u64 {
-    fn extract(string: &String) -> Result<u64, ExtractInfoError> {
-        use std::num::ParseIntError;
-
-        string.parse().map_err(|e: ParseIntError| ExtractInfoError::FromError(e.to_string()))
-    }
-}
-
 impl Extractable for Resolution {
-    fn extract(string: &String) -> Result<Resolution, ExtractInfoError> {
-        use std::num::ParseIntError;
+    fn extract(s: &String) -> Result<Resolution, BadValueError> {
+        let components: Vec<_> = s.split('x').collect();
 
-        let components: Vec<_> = string.split('x').collect();
         if components.len() < 2 {
-            Err(ExtractInfoError::FromError(String::from("Bad resolution value")))
+            Err(BadValueError::new(s, "Missing resolution component"))
         } else {
-            let width = components[0].parse()
-                .map_err(|e: ParseIntError| ExtractInfoError::FromError(e.to_string()))?;
-            let height = components[1].parse()
-                .map_err(|e: ParseIntError| ExtractInfoError::FromError(e.to_string()))?;
+            let parse_error = |e| {
+                let detail = format!("Invalid resolution component: {}", e);
+                BadValueError::new(s, &detail)
+            };
 
-            let resolution = Resolution { width: width, height: height };
+            let resolution = Resolution {
+                width: components[0].parse().map_err(parse_error)?,
+                height: components[1].parse().map_err(parse_error)?,
+            };
+
             Ok(resolution)
         }
     }
 }
 
 impl Extractable for MediaType {
-    fn extract(string: &String) -> Result<MediaType, ExtractInfoError> {
-        if string.to_lowercase() == "video" {
-            Ok(MediaType::Video)
-        } else {
-            Err(ExtractInfoError::FromError(String::from("Bad media type value")))
+    fn extract(s: &String) -> Result<MediaType, BadValueError> {
+        match &s.to_lowercase() as &str {
+            "video" => Ok(MediaType::Video),
+            _       => Err(BadValueError::new(s, "Invalid media type value"))
         }
     }
 }
