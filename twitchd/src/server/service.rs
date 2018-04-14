24e897e603b36f33c86ec4a60cc7f5267f666fa6
Guillaume Depardon <guillaume.depardon@gmail.com>
2018-04-14 19:14:35 +0200

    Added some routing logic

--- twitchd/src/server/service.rs
+++ twitchd/src/server/service.rs
@@ -1,43 +1,103 @@
 extern crate hyper;
 extern crate futures;
+extern crate url;
 
 use self::hyper::server;
 use self::futures::{Future, future};
 
-use super::state::State;
+use super::state::{index_cache::IndexError, State};
 
 use std::rc::Rc;
+use std::collections::HashMap;
 
 pub struct TwitchdApi {
     state: Rc<State>
 }
 
+type ApiRequest = server::Request;
+type ApiResponse = server::Response;
+type ApiError = hyper::Error;
+type ApiFuture = Box<Future<Item = ApiResponse, Error = ApiError>>;
+
+type QueryParams = HashMap<String, String>;
+
 impl TwitchdApi {
     pub fn new(state: &Rc<State>) -> Self {
         Self { state: Rc::clone(state) }
     }
+
+    fn get_stream_index(&self, channel: &str) -> ApiFuture {
+        let return_index = |index| {
+            server::Response::new()
+                .with_body(format!("{:#?}", index))
+        };
+
+        let return_error = |error| {
+            match error {
+                IndexError::NotFound          => not_found(),
+                IndexError::Unexpected(error) => server_error(&error)
+            }
+        };
+
+        let response = self.state.index_cache.get(channel)
+            .map(return_index)
+            .or_else(return_error);
+
+        Box::new(response)
+    }
 }
 
 impl server::Service for TwitchdApi {
-    type Request = server::Request;
-    type Response = server::Response;
-    type Error = hyper::Error;
-    type Future = Box<Future<Item = Self::Response, Error = Self::Error>>;
+    type Request = ApiRequest;
+    type Response = ApiResponse;
+    type Error = ApiError;
+    type Future = ApiFuture;
 
     fn call(&self, req: Self::Request) -> Self::Future {
-        let channel = &req.path()[1..];
-        println!("{}", channel);
+        use self::hyper::Method::Get;
+        use self::url::form_urlencoded::parse as parse_query;
 
-        let future = self.state.index_cache.get(channel)
-            .map(|index| {
-                hyper::server::Response::new()
-                    .with_body(format!("{:#?}", index))
-            })
-            .or_else(|error| {
-                future::ok(hyper::server::Response::new()
-                    .with_status(hyper::StatusCode::NotFound))
-            });
+        let query_bytes = req.query().unwrap_or_default().as_bytes();
+        let params: QueryParams = parse_query(query_bytes)
+            .map(|(k, v)| (String::from(k), String::from(v)))
+            .collect();
 
-        Box::new(future)
+        match (req.method(), req.path()) {
+            // Stream index
+            (Get, "/stream_index") => {
+                match params.get("channel") {
+                    None          => bad_request("Missing channel"),
+                    Some(channel) => self.get_stream_index(&channel)
+                }
+            },
+            // Default => 404
+            _ => not_found()
+        }
     }
+}
+
+fn not_found() -> ApiFuture {
+    use self::hyper::StatusCode::NotFound;
+
+    let response = server::Response::new()
+        .with_status(NotFound);
+    Box::new(future::ok(response))
+}
+
+fn bad_request(detail: &str) -> ApiFuture {
+    use self::hyper::StatusCode::BadRequest;
+
+    let response = server::Response::new()
+        .with_status(BadRequest)
+        .with_body(String::from(detail));
+    Box::new(future::ok(response))
+}
+
+fn server_error(detail: &str) -> ApiFuture {
+    use self::hyper::StatusCode::InternalServerError;
+
+    let response = server::Response::new()
+        .with_status(InternalServerError)
+        .with_body(String::from(detail));
+    Box::new(future::ok(response))
 }
