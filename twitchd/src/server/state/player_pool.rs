8a0ac5b6b409ea799e9bb7c864dd7f4d467a99ac
Guillaume Depardon <guillaume.depardon@gmail.com>
2018-04-15 16:59:21 +0200

    Added a player pool to share network resources

---
+++ twitchd/src/server/state/player_pool.rs
@@ -0,0 +1,71 @@
+extern crate futures;
+
+use types::Handle;
+
+use super::stream_player::{StreamPlayer, PlayerSink};
+use twitch::types::PlaylistInfo;
+
+use self::futures::Future;
+
+use std::rc::Rc;
+use std::cell::RefCell;
+use std::collections::HashMap;
+
+type Channel = String;
+type Quality = String;
+type Stream = (Channel, Quality);
+
+pub struct PlayerPool {
+    handle: Rc<Handle>,
+    players: Rc<RefCell<HashMap<Stream, StreamPlayer>>>,
+}
+
+impl PlayerPool {
+    pub fn new(handle: &Handle) -> Self {
+        Self {
+            handle: Rc::new(handle.clone()),
+            players: Rc::new(RefCell::new(HashMap::new())),
+        }
+    }
+
+    pub fn is_playing(&self, stream: &Stream) -> bool {
+        self.players.borrow().contains_key(&stream)
+    }
+
+    pub fn add_client(&self, stream: Stream, sink: PlayerSink) {
+        if let Some(player) = self.players.borrow().get(&stream) {
+            player.add_sink(sink)
+        }
+    }
+
+    pub fn add_player(&self, stream: Stream, playlist_info: PlaylistInfo, sink: PlayerSink) {
+        let new_player = {
+            let stream = stream.clone();
+            move || {
+                let player = StreamPlayer::new(playlist_info);
+                self.play(&player, stream);
+                player
+            }
+        };
+
+        self.players.borrow_mut()
+            .entry(stream)
+            .or_insert_with(new_player)
+            .add_sink(sink);
+    }
+
+    fn play(&self, player: &StreamPlayer, stream: Stream) {
+        let remove_player = {
+            let players = Rc::clone(&self.players);
+            move |result| {
+                players.borrow_mut().remove(&stream);
+                Ok(())
+            }
+        };
+
+        let future = player.play()
+            .then(remove_player);
+
+        self.handle.spawn(future);
+    }
+}
