be2eb8ded2bffe4dee52bd941dbcdfc9e18f659d
Guillaume Depardon <guillaume.depardon@gmail.com>
2018-04-21 00:24:18 +0200

    Dirty argument handling

--- player/src/main.cpp
+++ player/src/main.cpp
@@ -4,6 +4,8 @@
 #include <QApplication>
 #include <QProcess>
 
+#include "video_widget.hpp"
+
 int main(int argc, char *argv[]) {
     QProcess::startDetached(constants::TWITCHD_PATH, QStringList()
         << "--client-id" << constants::TWITCHD_CLIENT_ID
@@ -14,12 +16,24 @@ int main(int argc, char *argv[]) {
     auto args = app.arguments();
     MainWindow main_window;
 
-    if (args.size() >= 2)
-        main_window.add_stream(args[1]);
-    else
-        main_window.add_stream("lirik");
+    if (args.size() >= 2) {
+        if (args[1].startsWith("twitch-player://")) {
+            auto channel = args[1].remove("twitch-player://").remove("/");
+            main_window.add_stream(channel);
+        }
+        else
+            main_window.add_stream(args[1]);
+    }
+    // else
+    //     main_window.add_stream("lirik");
 
     main_window.show();
+
+    // libvlc::Instance i;
+    // VideoWidget w { i };
+    // w.play("lirik");
+    // w.set_volume(20);
+    // w.show();
 
     return app.exec();
 }
