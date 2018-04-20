3be52aa56addbf090784f6a6a888848c34962a50
Guillaume Depardon <guillaume.depardon@gmail.com>
2018-04-21 00:22:30 +0200

    Improved video widget
    Added basic volume control with an overlay widget

--- player/include/video_widget.hpp
+++ player/include/video_widget.hpp
@@ -5,6 +5,38 @@
 
 #include <optional>
 #include <QWidget>
+#include <QTimer>
+
+class VolumeOverlay: public QWidget
+{
+public:
+    VolumeOverlay(QWidget *parent = nullptr):
+        QWidget(parent)
+    {
+        setWindowFlags(Qt::Window | Qt::FramelessWindowHint);
+        setAttribute(Qt::WA_NoSystemBackground);
+        setAttribute(Qt::WA_TranslucentBackground);
+        setAttribute(Qt::WA_PaintOnScreen);
+
+        setAttribute(Qt::WA_TransparentForMouseEvents);
+
+        timer.setInterval(2500);
+        QObject::connect(&timer, &QTimer::timeout, [this] {
+            text.reset();
+            repaint();
+        });
+    }
+
+    void show_volume(int);
+    void show_muted(bool);
+
+protected:
+    void paintEvent(QPaintEvent *) override;
+
+private:
+    std::optional<QString> text;
+    QTimer timer;
+};
 
 class VideoWidget: public QWidget
 {
@@ -12,21 +44,25 @@ public:
     VideoWidget(libvlc::Instance &, QWidget * = nullptr);
 
     void play(QString);
-    void set_volume(int volume);
+    void set_volume(int);
+    void set_muted(bool);
 
+    void update_overlay_position();
+
+protected:
+    void wheelEvent(QWheelEvent *) override;
+    void keyPressEvent(QKeyEvent *) override;
+    void moveEvent(QMoveEvent *) override;
+    void resizeEvent(QResizeEvent *) override;
+    void showEvent(QShowEvent *) override;
 private:
     libvlc::Instance & _instance;
     libvlc::MediaPlayer _media_player;
     std::optional<libvlc::Media> _media;
+    VolumeOverlay *_overlay;
+
+    int _vol = 35;
+    bool _muted = false;
 };
 
 #endif // VIDEO_WIDGET_HPP
-
-//QWidget *overlay;
-//overlay = new W(&w);
-//overlay->setWindowFlags(Qt::Window | Qt::FramelessWindowHint);
-//overlay->setAttribute(Qt::WA_NoSystemBackground);
-//overlay->setAttribute(Qt::WA_TranslucentBackground);
-//overlay->setAttribute(Qt::WA_PaintOnScreen);
-//overlay->setWindowOpacity(1);
-//overlay->show();
--- player/src/video_widget.cpp
+++ player/src/video_widget.cpp
@@ -1,10 +1,24 @@
 #include "video_widget.hpp"
 
+#include <QWheelEvent>
+#include <QDebug>
+#include <QPainter>
+#include <QPen>
+#include <QFont>
+#include <QTimer>
+
 VideoWidget::VideoWidget(libvlc::Instance &instance, QWidget *parent):
-    QWidget(parent), _instance(instance), _media_player(libvlc::MediaPlayer(instance))
+    QWidget(parent),
+    _instance(instance),
+    _media_player(libvlc::MediaPlayer(instance)),
+    _overlay(new VolumeOverlay(this))
 {
     setAttribute(Qt::WA_OpaquePaintEvent);
+    setFocusPolicy(Qt::StrongFocus);
     _media_player.set_renderer((void *)winId());
+    set_volume(_vol);
+    _overlay->show();
+    update_overlay_position();
 }
 
 void VideoWidget::play(QString channel) {
@@ -15,5 +29,81 @@ void VideoWidget::play(QString channel) {
 }
 
 void VideoWidget::set_volume(int volume) {
-    _media_player.set_volume(volume);
+    _vol = volume;
+    _media_player.set_volume(_muted ? 0 : _vol);
+    _overlay->show_volume(_vol);
+}
+
+void VideoWidget::set_muted(bool muted) {
+    _muted = muted;
+    set_volume(_vol);
+    _overlay->show_muted(muted);
+}
+
+void VideoWidget::update_overlay_position() {
+    QPoint p = mapToGlobal(pos());
+    _overlay->resize(size());
+    _overlay->move(p);
+    _overlay->raise();
+}
+
+void VideoWidget::wheelEvent(QWheelEvent *event) {
+    int new_volume;
+    if (event->angleDelta().y() > 0)
+        new_volume = std::min(_vol + 5, 100);
+    else
+        new_volume = std::max(0, _vol - 5);
+    set_volume(new_volume);
+    QWidget::wheelEvent(event);
+}
+
+void VideoWidget::keyPressEvent(QKeyEvent *event) {
+    qDebug() << event;
+    if (event->key() == Qt::Key_M) {
+        set_muted(!_muted);
+    }
+
+    QWidget::keyPressEvent(event);
+}
+
+void VideoWidget::moveEvent(QMoveEvent *event) {
+    update_overlay_position();
+}
+
+void VideoWidget::resizeEvent(QResizeEvent *event) {
+    update_overlay_position();
+}
+
+void VideoWidget::showEvent(QShowEvent *event) {
+    // have to actually wait for it to be shown...
+    QTimer::singleShot(250, [this] {
+        update_overlay_position();
+    });
+}
+
+void VolumeOverlay::paintEvent(QPaintEvent *event) {
+    if (text) {
+        QPainter painter { this };
+        QPen pen;
+        QFont font;
+        font.setBold(true);
+        font.setPointSize(40);
+        font.setWeight(40);
+        painter.setFont(font);
+        painter.setPen(Qt::white);
+
+        painter.drawText(rect(), Qt::AlignTop | Qt::AlignRight, *text);
+    }
+}
+
+void VolumeOverlay::show_volume(int volume) {
+    text.emplace(QString::number(volume) + " %");
+    repaint();
+    timer.start();
+}
+
+void VolumeOverlay::show_muted(bool muted) {
+    text.emplace(muted ? "Muted" : "Unmuted");
+    repaint();
+    timer.start();
 }
