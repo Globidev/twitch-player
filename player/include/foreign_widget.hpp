2aaaf919ec169f41cad53e7d805a2a076f37bc8f
Guillaume Depardon <guillaume.depardon@gmail.com>
2018-04-21 16:19:59 +0200

    Now correctly closing chrome's popout

--- player/include/foreign_widget.hpp
+++ player/include/foreign_widget.hpp
@@ -12,8 +12,9 @@ struct ForeignWidget: public QWidget {
   private:
     QWindow *_win;
     QWidget *_container;
-    QHBoxLayout *_layout;
+    QHBoxLayout _layout;
     QRect _win_geometry;
+    void * _handle;
 };
 
 #endif // FOREIGN_WIDGET_HPP
