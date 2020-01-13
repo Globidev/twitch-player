b0bd250f05514624f52ad9f5dfe035207e408913
Guillaume Depardon <guillaume.depardon@gmail.com>
2020-01-13 01:00:04 +0100

    Simplified top level server logic

--- twitchd/src/server/mod.rs
+++ twitchd/src/server/mod.rs
@@ -20,42 +20,51 @@ pub fn run(opts: Options) {
 
     let addr = SocketAddr::new(opts.host, opts.port);
 
-    let (shutdown_sender, shutdown_receiver) = oneshot::channel();
-    let state = State::new(opts, shutdown_sender);
+    let (shutdown_tx, shutdown_rx) = oneshot::channel();
+    let state = State::new(opts, shutdown_tx);
     let shared_state = Arc::new(state);
 
     let make_service = make_service_fn(move |_| {
         let shared_state = shared_state.clone();
         async move {
-            Ok::<_, Infallible>(service_fn(move |req| {
+            let service = service_fn(move |req| {
                 let shared_state = shared_state.clone();
                 async move {
-                    let mut api = TwitchdApi::new(&shared_state);
-                    Ok::<_, Infallible>(api.call(req).await)
+                    let api = TwitchdApi::new(&shared_state);
+                    let response = api.call(req).await;
+                    infallible(response)
                 }
-            }))
+            });
+
+            infallible(service)
         }
     });
 
+    let serve = async {
+        let server = hyper::Server::bind(&addr)
+            .serve(make_service);
+
+        let server_with_shutdown = future::select(
+            server.map_err(ServerError::Hyper),
+            shutdown_rx.map_err(ServerError::ShutdownCanceled)
+        );
+
+        let (serve_result, _) = server_with_shutdown.await.factor_first();
+        serve_result
+    };
+
     runtime
-        .block_on(async {
-            let server = hyper::Server::bind(&addr)
-                .serve(make_service);
-
-            let server_with_shutdown = future::select(
-                server.map_err(ServerError::Hyper),
-                shutdown_receiver.map_err(ServerError::ShutdownCanceled)
-            );
-
-            let (serve_result, _) = server_with_shutdown.await.factor_first();
-            serve_result
-        })
+        .block_on(serve)
         .expect("Failed to run server");
+}
+
+fn infallible<T>(x: T) -> Result<T, Infallible> {
+    Ok(x)
 }
 
 #[derive(Debug, Error)]
 enum ServerError {
-    #[error("Lost shutdown capability somwhow")]
+    #[error("Lost shutdown capability somewhow")]
     ShutdownCanceled(oneshot::Canceled),
     #[error("Server error: {0}")]
     Hyper(hyper::Error)
